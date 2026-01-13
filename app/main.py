55e0bb0394d94b745d2db036ae7d92d4ee36fafc
Amin Mousavi <ma.mousavi97@gmail.com>
2026-01-13 14:52:40 +0200

    implemented respond to PING command

--- app/main.py
+++ app/main.py
@@ -4,15 +4,21 @@ from app.logger import setup_logging, get_logger
 # Initialize logger for this module
 logger = get_logger(__name__)
 
+SERVER_ADDRESS = "localhost"
+SERVER_PORT = 6379
 
 def main():
     setup_logging()
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     logger.info("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server((SERVER_ADDRESS, SERVER_PORT), reuse_port=True) as server_socket:
+        connection, address = server_socket.accept() # wait for client
+        logger.info("Connection received from: %s", address)
+        data = connection.recv(1024)
+        logger.info("Received data: %s", data)
+        if b"PING" in data:
+            connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
