6c6825483fb6309de7e6e24d1dad6d5e9eb95fea
Amin Mousavi <ma.mousavi97@gmail.com>
2026-01-14 23:44:29 +0200

    Event handling on sockets

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import socket  # noqa: F401
 from app.logger import setup_logging, get_logger
+import select
 
-# Initialize logger for this module
 logger = get_logger(__name__)
 
 SERVER_ADDRESS = "localhost"
@@ -9,21 +9,32 @@ SERVER_PORT = 6379
 
 def main():
     setup_logging()
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+
     logger.info("Starting the server...")
 
     with socket.create_server((SERVER_ADDRESS, SERVER_PORT), reuse_port=True) as server_socket:
+        server_socket.setblocking(False)
+        sockets_list = [server_socket]
+
         while True:
-            connection, address = server_socket.accept() # wait for client
-            logger.info("Connection received from: %s", address)
-            while True:
-                data = connection.recv(1024)
-                logger.info("Received data: %s", data)
-                if b"PING" in data:
-                    connection.sendall(b"+PONG\r\n")
-                elif b"" == data:
-                    logger.info("Client disconnected")
-                    break
+            ready_to_read, _, _ = select.select(sockets_list, [], [])
+            
+            for ready_socket in ready_to_read:
+                
+                if ready_socket == server_socket:
+                    connection, _ = ready_socket.accept()
+                    logger.info("Connection received from %s", connection)
+                    sockets_list.append(connection)
+                else:
+                    data = ready_socket.recv(1024)
+                    
+                    # Empty data
+                    if data == b"":
+                        logger.info("Client %s disconnected", ready_socket)
+                        sockets_list.remove(ready_socket)
+                        ready_socket.close()
+                    elif b"PING" in data:
+                        ready_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
