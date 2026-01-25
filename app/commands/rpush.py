f4539d050dba71734cb7dd8d385fc68b6e2aa811
Amin Mousavi <ma.mousavi97@gmail.com>
2026-01-25 03:32:59 +0200

    Supported multiple value appending to a list

--- app/commands/rpush.py
+++ app/commands/rpush.py
@@ -3,17 +3,18 @@ from app.commands.base import Command
 
 class RPushCommand(Command):
     name = "RPUSH"
-    arity = (2, 2)
+    arity = (2, float('inf'))
 
     def __init__(self):
         self.lists: dict[str, list] = {}
     
     def execute(self, args: list[str]) -> int:
         list_name = args[0]
-        list_value = args[1]
+        list_values = args[1:]
         
         if list_name not in self.lists:
             self.lists[list_name] = []
         
-        self.lists[list_name].append(list_value)
+        for value in list_values:
+            self.lists[list_name].append(value)
         return len(self.lists[list_name])
